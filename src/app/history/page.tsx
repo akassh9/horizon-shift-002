a0c4cc04c2e20d456f49855d45c838031445bb10
akassh9 <khanikad@mail.uc.edu>
2025-05-03 01:19:31 +0530

    F

--- src/app/history/page.tsx
+++ src/app/history/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+export const dynamic = 'force-dynamic';
 import { useSearchParams } from "next/navigation";
 import React, { Suspense } from "react";
 import { DraggableWindow } from "../../../components/DraggableWindow";
